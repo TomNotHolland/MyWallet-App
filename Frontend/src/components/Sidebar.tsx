28363bf5120a3ce411ad68ea2d694c3aa28cb568
Tom <wintonmello4@gmail.com>
2022-10-21 01:59:47 -0300

    Sidebar file syntax and size reduced by splitting up few parts into a components

--- Frontend/src/components/Sidebar.tsx
+++ Frontend/src/components/Sidebar.tsx
@@ -1,37 +1,33 @@
 import { Cardholder, List } from "phosphor-react";
 import { useMenu } from '../hooks/MenuHook';
 import Accordion from "./Accordion";
+import { AccordionOption } from "./AccordionOption";
+import NavHeader from "./NavHeader";
 
 export default function Sidebar() {
   const { openMenu, setOpenMenu } = useMenu()
 
   return (
     <section className="flex">
-      <aside className={`flex flex-col relative bg-zinc-900 min-h-screen gap-y-4 ${openMenu ? "w-16" : "w-72"} duration-500`}>
-        <div className="inline-flex w-[90%] justify-between">
-          <a href="#" className={`cursor-pointer select-none 
-            ${!openMenu ? "inline-flex justify-start mt-4 ml-4 text-yellow-500 text-xl"
-              : "flex mt-4 opacity-0 overflow-hidden duration-500"}`}>
+      <aside className={`flex flex-col relative bg-zinc-900 min-h-screen gap-y-5 ${openMenu ? "w-16" : "w-72"} duration-500`}>
+        <div className="flex justify-between">
+          <a href="#" className={`flex cursor-pointer select-none mt-4 duration-500
+            ${!openMenu ? "justify-start ml-4 text-yellow-500 text-xl"
+              : "opacity-0 overflow-hidden"}`}>
             MyWallet
             <Cardholder size={32} weight="light" className="ml-3 min-w-fit" />
           </a>
           <div className={`${!openMenu ? "self-end" : "self-end mr-3"}`} onClick={() => setOpenMenu(!openMenu)}>
-            <List size={32} weight="regular"
-              className={`${!openMenu ? "text-yellow-500 cursor-pointer duration-500 translate-x-0"
-                : "text-yellow-500 cursor-pointer duration-500 -translate-x-0"}`} />
+            <List size={32} className={`duration-500 text-yellow-500 cursor-pointer 
+            ${openMenu ? "-translate-x-1"
+              : "-translate-x-3"}`} />
           </div>
         </div>
-        <Accordion />
+        <Accordion>
+          <AccordionOption />
+        </Accordion>
       </aside>
-
-      <nav className="inline-flex h-16 bg-yellow-600 w-full text-zinc-900 font-semibold">
-        <span
-          className={`${!openMenu ? "flex invisible overflow-hidden opacity-0 duration-200 translate-x-32"
-            : "select-none overflow-hidden flex text-2xl m-4 duration-300"}`}>
-          MyWallet
-          <Cardholder size={32} className="mx-2 my-0.5" />
-        </span>
-      </nav>
+      <NavHeader />
     </section>
   );
 }
