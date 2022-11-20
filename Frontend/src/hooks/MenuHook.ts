c62baf0b7feb853e46b86a2e2a5a424738f80085
Tom <84048957+TomNotHolland@users.noreply.github.com>
2022-11-20 02:41:22 -0300

    Deleted custom hook

--- Frontend/src/hooks/MenuHook.ts
+++
@@ -1,9 +0,0 @@
-import { MenuState, Context } from '../states/MenuState';
-import { useContext } from 'react'
-
-//? Custom Hook
-
-export function useMenu(): Context {
-  const value = useContext(MenuState)
-  return value
-}
