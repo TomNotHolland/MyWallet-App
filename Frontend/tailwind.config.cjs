4ce03e2e782a27792c4a9ed8ac16fb71fd63e9bd
Tom <wintonmello4@gmail.com>
2022-11-24 08:57:25 -0300

    Created customised reponsive screen breaking spots

--- Frontend/tailwind.config.cjs
+++ Frontend/tailwind.config.cjs
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.html', './src/components/*.tsx',
-    './src/*tsx.'
+    './index.html',
+    './src/components/*.tsx',
+    './src/components/pages/*.tsx',
+    './src/routes/*tsx.',
+    './src/main/*.tsx'
   ],
   theme: {
-    extend: {},
+    screens: {
+      'mobile': {'min': '0px', 'max': '767px'},
+      'tablet': {'min': '768px', 'max': '1023px'},
+      'laptop': {'min': '1024px', 'max': '1279px'},
+      'lg-laptop': {'min': '1280px', 'max': '1535px'},
+      'desktop': {'min': '1536px'}
+    }
   },
   plugins: [],
 }
