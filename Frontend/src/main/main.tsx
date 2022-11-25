6045b4b778563d161fdff28f61cb411d89d69297
Tom <wintonmello4@gmail.com>
2022-11-24 21:51:37 -0300

    Wrapped App component with Redux Provider

--- Frontend/src/main/main.tsx
+++ Frontend/src/main/main.tsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import React from 'react';
+import React from 'react'
+import { Provider } from 'react-redux'
+import { ReduxStore } from './app/Store';
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
       <Router>
-        <App />
+        <Provider store={ReduxStore}>
+          <App />
+        </Provider>
       </Router>
     </React.StrictMode>
   )
