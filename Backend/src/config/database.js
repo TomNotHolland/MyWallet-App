3511e83dfce558a31f3e87ec2e1ec5b26c93f061
Tom <wintonmello4@gmail.com>
2022-12-13 23:08:10 -0300

    Fixed MongoDB connection error with node-restful

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mywalletapp', {useNewUrlParser: true})
+module.exports = mongoose.connect('mongodb://0.0.0.0/mywalletapp', {useNewUrlParser: true})
+
+//! When finally is using Restify, swap '0.0.0.0' for 'localhost'
