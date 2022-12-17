55c636946c62f8926b45c72b0d11d7bfb2644c77
Tom <wintonmello4@gmail.com>
2022-12-17 01:28:17 -0300

    Added new reducer and edited DevTools type

--- Frontend/src/main/app/Store.ts
+++ Frontend/src/main/app/Store.ts
@@ -1,19 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit"
-import DashboardSlice from '../../components/pages/Dashboard/DashboardSlice';
-import TabSlice from "../../components/pages/BillingCycle/TabSlice";
+import { configureStore } from '@reduxjs/toolkit'
+import DashboardSlice from '../../components/pages/Dashboard/DashboardSlice'
+import TabSlice from '../../components/pages/BillingCycle/TabSlice'
+import BillingCycleSlice from '../../components/pages/BillingCycle/BillingCycleSlice'
 
 declare global {
-	interface Window {
-		__REDUX_DEVTOOLS_EXTENSION__?: any;
+	interface Window {A
+		__REDUX_DEVTOOLS_EXTENSION__?: () => boolean | undefined
 	}
 }
 
-const DevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const DevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 export const ReduxStore = configureStore({
 	reducer: {
     dashboard: DashboardSlice.reducer,
-    tab: TabSlice.reducer
+    tab: TabSlice.reducer,
+    billingcycle: BillingCycleSlice.reducer,
   },
   devTools: DevTools
 })
