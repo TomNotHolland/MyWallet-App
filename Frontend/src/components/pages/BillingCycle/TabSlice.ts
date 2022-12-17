b48e159aae405eee43102cc6360d8416ee7c1e1a
Tom <wintonmello4@gmail.com>
2022-12-17 01:41:50 -0300

    perf: Removed unused slice initial state

--- Frontend/src/components/pages/BillingCycle/TabSlice.ts
+++ Frontend/src/components/pages/BillingCycle/TabSlice.ts
@@ -1,26 +1,25 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
 
-export function TabSelected(TabId: string) {
-  return {
-    type: 'TAB_SELECTED',
-    payload: TabId
-  }
+export function selectTab(TabId: string) {
+	return {
+		type: 'TAB_SELECTED',
+		payload: TabId,
+	}
 }
 
 const TabSlice = createSlice({
-  name: 'tab',
-  initialState: {
-    selectStatus: `${name}`
-  },
-  reducers: {
-    TabDefault: (state, action: PayloadAction<string>) => {
-      switch (action.type) {
-        case 'TAB_SELECTED':
-          return { ...state, selectStatus: action.type }
-        default: return state
-      }
-    }
-  }
+	name: 'tab',
+	initialState: {},
+	reducers: {
+		TabDefault: (state, action: PayloadAction<string>) => {
+			switch (action.type) {
+				case 'TAB_SELECTED':
+					return { ...state , selectStatus: action.payload }
+				default:
+					return state
+			}
+		},
+	},
 })
 
 export default TabSlice
